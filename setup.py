f1c8e9e0a8fe6b024bf21344ecee575370972e55
Daniel Dwyer-Westwood <163413562+danielwestwoodABUHB@users.noreply.github.com>
2026-04-07 11:51:21 +0100

    Update version to 0.1.7 for setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="abspc",
-    version="0.1.6",
+    version="0.1.7",
     author="Aneurin Bevan University Health Board, Daniel Westwood",
     author_email="daniel.westwood@wales.nhs.uk",
     description=(
